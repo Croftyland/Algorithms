2fb06c36dab4611e8d774775fb109243295fbdad
Christina Landvytovych <Crofty.land@gmail.com>
2018-10-20 21:12:25 +0300

    counting up and down

---
+++ Masterclass/goingUpAndDown.js
@@ -0,0 +1,11 @@
+function countUpAndDown(n) {
+    console.log("Going up!");
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+    }
+    console.log("At the top!\nGoing down...");
+    for (let j = n - 1; j >= 0; j--) {
+        console.log(j);
+    }
+    console.log("Back down. Bye!");
+}
