e7b0cb0df06bc0cc91cc687f6308ec7434d629e9
Christina Landvytovych <Crofty.land@gmail.com>
2018-10-23 01:58:55 +0300

    vasia problem v2

--- codeWars/vasiaClerk.js
+++ codeWars/vasiaClerk.js
@@ -25,3 +25,36 @@ function tickets(peopleInLine) {
     }
     return "YES";
 }
+
+
+function Clerk(name) {
+    this.name = name;
+
+    this.money = {
+        25 : 0,
+        50 : 0,
+        100: 0
+    };
+
+    this.sell = function(element, index, array) {
+        this.money[element]++;
+
+        switch (element) {
+            case 25:
+                return true;
+            case 50:
+                this.money[25]--;
+                break;
+            case 100:
+                this.money[50] ? this.money[50]-- : this.money[25] -= 2;
+                this.money[25]--;
+                break;
+        }
+        return this.money[25] >= 0;
+    };
+}
+
+function tickets(peopleInLine){
+    var vasya = new Clerk("Vasya");
+    return peopleInLine.every(vasya.sell.bind(vasya)) ? "YES" : "NO";
+}
