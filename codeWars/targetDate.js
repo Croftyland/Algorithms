d260e77c6040042f83225738e5ff37514b4b66c4
Christina Landvytovych <Crofty.land@gmail.com>
2019-01-01 15:02:45 +0200

    Target date (another solution)

--- codeWars/targetDate.js
+++ codeWars/targetDate.js
@@ -47,4 +47,12 @@ function dateNbDays(a0, a, p) {
 
 }
 
+//another solution
 
+const ln = Math.log;
+
+function dateNbDays(a0, a, p) {
+    let days = Math.ceil((ln(a) - ln(a0))/ln(1 + p/36000));
+    var date = new Date(2016, 0, 1 + days);
+    return date.toISOString().split('T')[0];
+}
